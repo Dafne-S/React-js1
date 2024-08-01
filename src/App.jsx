c81ca8c4ba7ff9d60796b3861434ac12d9960d4f
Dafne Serantes <ds.dafneserantes@gmail.com>
2024-08-01 15:48:46 -0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic architecto nobis, ipsa magni omnis maiores blanditiis fugit obcaecati, quos ratione beatae dignissimos illum natus ipsam saepe dolorum totam sequi laborum?</p>
+      <h1>hola</h1>
+    </>
+  )
+}
+
+export default App
