efd4c90d0668ebe90a94abef3505df2028f9d06c
Dafne Serantes <ds.dafneserantes@gmail.com>
2024-08-01 17:44:37 -0300

    modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/components/Navbar.js/Navbar.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
 import "./App.css"
+import Navbar  from "./components/Navbar.js/Navbar"
 
 function App() {
 
+  const divStyle = {
+    color: 'violet',
+    backgroundColor: 'lightgray',
+    padding: '10px',
+    border: '1px solid black',
+  }
+
   return (
     <>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic architecto nobis, ipsa magni omnis maiores blanditiis fugit obcaecati, quos ratione beatae dignissimos illum natus ipsam saepe dolorum totam sequi laborum?</p>
-      <h1>hola</h1>
+      <h1 style={divStyle}> React </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam laudantium similique eligendi voluptas sunt harum vitae, tenetur adipisci nihil alias sint, mollitia repellat nostrum suscipit molestias quaerat perferendis non porro?</p>
+      <Navbar></Navbar>
     </>
   )
 }
---
+++ src/components/Navbar.js/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return(
+        <>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ut harum quam veniam corporis repellat animi ratione eaque! Sint quas eaque, odio nobis ipsa perspiciatis? Molestiae voluptatem laudantium praesentium ullam!</p>
+        </>
+    )
+}
+
+export default Navbar;
